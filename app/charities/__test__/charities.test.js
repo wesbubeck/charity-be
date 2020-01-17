fd69283a36ef7904df4e18e26b22b10b5fca6619
Denis Molloy <denis.molloy@gmail.com>
2020-01-16 22:24:20 -0800

    package.json->modified test script to include --runInBand

--- app/charities/__test__/charities.test.js
+++ app/charities/__test__/charities.test.js
@@ -3,98 +3,105 @@
 const mongoose = require('mongoose');
 const Charity = require('../charity-queries');
 
-
 describe('Charities', () => {
-    let createdCharityOne;
-    let createdCharityTwo;
-    const charityDataOne = {
-        address: '123 beet farm ave',
-        email: 'nard.dog@dunder.com',
-        charityName: 'Conn Casual Chaps',
-    };
-    const charityDataTwo = {
-        address: '134 dunder way',
-        email: 'm.scott@dunder.com2',
-        charityName: 'Scotts tots',
+  let createdCharityOne;
+  let createdCharityTwo;
+  const charityDataOne = {
+    address: '123 beet farm ave',
+    email: 'nard.dog@dunder.com',
+    charityName: 'Conn Casual Chaps'
+  };
+  const charityDataTwo = {
+    address: '134 dunder way',
+    email: 'm.scott@dunder.com2',
+    charityName: 'Scotts tots'
+  };
 
-    };
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  });
 
-    beforeAll(async () => {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  afterAll(async () => {
+    const collections = await mongoose.connection.db.collections();
+
+    collections.forEach(async collection => {
+      await collection.remove();
     });
 
-    afterAll(async () => {
-        const collections = await mongoose.connection.db.collections();
+    await mongoose.connection.close();
+  });
 
-        collections.forEach(async (collection) => {
-            await collection.remove();
-        });
-
-        await mongoose.connection.close();
+  test('should create a new charity', async () => {
+    createdCharityOne = await Charity.createCharity({
+      charityName: charityDataOne.charityName,
+      address: charityDataOne.address,
+      email: charityDataOne.email
     });
 
-    test('should create a new charity', async () => {
-        createdCharityOne = await Charity.createCharity({
-            charityName: charityDataOne.charityName,
-            address: charityDataOne.address,
-            email: charityDataOne.email,
-        });
+    expect(createdCharityOne.charityName).toBe(charityDataOne.charityName);
+    expect(createdCharityOne.email).toBe(charityDataOne.email);
+    expect(createdCharityOne.address).toBe(charityDataOne.address);
+  });
 
-        expect(createdCharityOne.charityName).toBe(charityDataOne.charityName);
-        expect(createdCharityOne.email).toBe(charityDataOne.email);
-        expect(createdCharityOne.address).toBe(charityDataOne.address);
+  test('should create another new charity', async () => {
+    createdCharityTwo = await Charity.createCharity({
+      charityName: charityDataTwo.charityName,
+      address: charityDataTwo.address,
+      email: charityDataTwo.email
     });
 
-    test('should create another new charity', async () => {
-        createdCharityTwo = await Charity.createCharity({
-            charityName: charityDataTwo.charityName,
-            address: charityDataTwo.address,
-            email: charityDataTwo.email,
-        });
+    expect(createdCharityTwo.charityName).toBe(charityDataTwo.charityName);
+    expect(createdCharityTwo.email).toBe(charityDataTwo.email);
+    expect(createdCharityTwo.address).toBe(charityDataTwo.address);
+  });
 
-        expect(createdCharityTwo.charityName).toBe(charityDataTwo.charityName);
-        expect(createdCharityTwo.email).toBe(charityDataTwo.email);
-        expect(createdCharityTwo.address).toBe(charityDataTwo.address);
-    });
+  test('should get a charity by id', async () => {
+    const getCharityTwo = await Charity.getCharityById(createdCharityTwo._id);
 
-    test('should get a charity by id', async () => {
-        const getCharityTwo = await Charity.getCharityById(createdCharityTwo._id);
+    expect(getCharityTwo.charityName).toBe(charityDataTwo.charityName);
+    expect(getCharityTwo.email).toBe(charityDataTwo.email);
+    expect(getCharityTwo.address).toBe(charityDataTwo.address);
+  });
 
-        expect(getCharityTwo.charityName).toBe(charityDataTwo.charityName);
-        expect(getCharityTwo.email).toBe(charityDataTwo.email);
-        expect(getCharityTwo.address).toBe(charityDataTwo.address);
-    });
+  test('should get all charities', async () => {
+    const allCharities = await Charity.getAllCharities();
+    const charityIds = allCharities.map(charity => charity._id);
 
-    test('should get all charities', async () => {
-        const allCharities = await Charity.getAllCharities();
-        const charityIds = allCharities.map((charity) => charity._id);
+    expect(allCharities).toHaveLength(2);
+    expect(charityIds).toEqual(
+      expect.arrayContaining([createdCharityTwo._id, createdCharityOne._id])
+    );
+  });
 
-        expect(allCharities).toHaveLength(2);
-        expect(charityIds)
-            .toEqual(expect.arrayContaining([createdCharityTwo._id, createdCharityOne._id]));
-    });
+  test('should update a charity by id', async () => {
+    const updatedCharityTwo = await Charity.updateCharityById(
+      createdCharityTwo._id,
+      {
+        email: 'new.email@test.com'
+      }
+    );
 
-    test('should update a charity by id', async () => {
-        const updatedCharityTwo = await Charity.updateCharityById(createdCharityTwo._id, {
-            email: 'new.email@test.com',
-        });
+    expect(updatedCharityTwo.charityName).toBe(charityDataTwo.charityName);
+    expect(updatedCharityTwo.email).toBe('new.email@test.com');
+    expect(updatedCharityTwo.address).toBe(charityDataTwo.address);
+  });
 
-        expect(updatedCharityTwo.charityName).toBe(charityDataTwo.charityName);
-        expect(updatedCharityTwo.email).toBe('new.email@test.com');
-        expect(updatedCharityTwo.address).toBe(charityDataTwo.address);
-    });
+  test('should delete a charity by id', async () => {
+    await Charity.removeCharityById(createdCharityTwo._id);
+    const allCharitiesAfterDelete = await Charity.getAllCharities();
+    const charityIdsAfterDelete = allCharitiesAfterDelete.map(
+      charity => charity._id
+    );
 
-    test('should delete a charity by id', async () => {
-        await Charity.removeCharityById(createdCharityTwo._id);
-        const allCharitiesAfterDelete = await Charity.getAllCharities();
-        const charityIdsAfterDelete = allCharitiesAfterDelete.map((charity) => charity._id);
-
-        expect(allCharitiesAfterDelete).toHaveLength(1);
-        expect(charityIdsAfterDelete)
-            .not.toEqual(expect.arrayContaining([createdCharityTwo._id, createdCharityOne._id]));
-        expect(charityIdsAfterDelete).toEqual(expect.arrayContaining([createdCharityOne._id]));
-    });
+    expect(allCharitiesAfterDelete).toHaveLength(1);
+    expect(charityIdsAfterDelete).not.toEqual(
+      expect.arrayContaining([createdCharityTwo._id, createdCharityOne._id])
+    );
+    expect(charityIdsAfterDelete).toEqual(
+      expect.arrayContaining([createdCharityOne._id])
+    );
+  });
 });
--- app/users/__test__/users.test.js
+++ app/users/__test__/users.test.js
@@ -3,98 +3,100 @@
 const mongoose = require('mongoose');
 const User = require('../user-queries');
 
-
 describe('Users', () => {
-    let createdUserOne;
-    let createdUserTwo;
-    const userDataOne = {
-        firstName: 'Dwight',
-        lastName: 'Schrute',
-        email: 'beet.farmer@dunder.com',
-    };
-    const userDataTwo = {
-        firstName: 'Pam',
-        lastName: 'Beasley',
-        email: 'pamelamadingdong@dunder.com2',
+  let createdUserOne;
+  let createdUserTwo;
+  const userDataOne = {
+    firstName: 'Dwight',
+    lastName: 'Schrute',
+    email: 'beet.farmer@dunder.com'
+  };
+  const userDataTwo = {
+    firstName: 'Pam',
+    lastName: 'Beasley',
+    email: 'pamelamadingdong@dunder.com2'
+  };
 
-    };
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  });
 
-    beforeAll(async () => {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+  afterAll(async () => {
+    const collections = await mongoose.connection.db.collections();
+
+    collections.forEach(async collection => {
+      await collection.remove();
     });
 
-    afterAll(async () => {
-        const collections = await mongoose.connection.db.collections();
+    await mongoose.connection.close();
+  });
 
-        collections.forEach(async (collection) => {
-            await collection.remove();
-        });
-
-        await mongoose.connection.close();
+  test('should create a new user', async () => {
+    createdUserOne = await User.createUser({
+      firstName: userDataOne.firstName,
+      lastName: userDataOne.lastName,
+      email: userDataOne.email
     });
 
-    test('should create a new user', async () => {
-        createdUserOne = await User.createUser({
-            firstName: userDataOne.firstName,
-            lastName: userDataOne.lastName,
-            email: userDataOne.email,
-        });
+    expect(createdUserOne.firstName).toBe(userDataOne.firstName);
+    expect(createdUserOne.email).toBe(userDataOne.email);
+    expect(createdUserOne.lastName).toBe(userDataOne.lastName);
+  });
 
-        expect(createdUserOne.firstName).toBe(userDataOne.firstName);
-        expect(createdUserOne.email).toBe(userDataOne.email);
-        expect(createdUserOne.lastName).toBe(userDataOne.lastName);
+  test('should create another new user', async () => {
+    createdUserTwo = await User.createUser({
+      firstName: userDataTwo.firstName,
+      lastName: userDataTwo.lastName,
+      email: userDataTwo.email
     });
 
-    test('should create another new user', async () => {
-        createdUserTwo = await User.createUser({
-            firstName: userDataTwo.firstName,
-            lastName: userDataTwo.lastName,
-            email: userDataTwo.email,
-        });
+    expect(createdUserTwo.firstName).toBe(userDataTwo.firstName);
+    expect(createdUserTwo.email).toBe(userDataTwo.email);
+    expect(createdUserTwo.lastName).toBe(userDataTwo.lastName);
+  });
 
-        expect(createdUserTwo.firstName).toBe(userDataTwo.firstName);
-        expect(createdUserTwo.email).toBe(userDataTwo.email);
-        expect(createdUserTwo.lastName).toBe(userDataTwo.lastName);
+  test('should get a user by id', async () => {
+    const getUserTwo = await User.getUserById(createdUserTwo._id);
+
+    expect(getUserTwo.firstName).toBe(userDataTwo.firstName);
+    expect(getUserTwo.email).toBe(userDataTwo.email);
+    expect(getUserTwo.lastName).toBe(userDataTwo.lastName);
+  });
+
+  test('should get all users', async () => {
+    const allUsers = await User.getAllUsers();
+    const userIds = allUsers.map(user => user._id);
+
+    expect(allUsers).toHaveLength(2);
+    expect(userIds).toEqual(
+      expect.arrayContaining([createdUserTwo._id, createdUserOne._id])
+    );
+  });
+
+  test('should update a user by id', async () => {
+    const updatedUserTwo = await User.updateUserById(createdUserTwo._id, {
+      email: 'new.email@test.com'
     });
 
-    test('should get a user by id', async () => {
-        const getUserTwo = await User.getUserById(createdUserTwo._id);
+    expect(updatedUserTwo.firstName).toBe(userDataTwo.firstName);
+    expect(updatedUserTwo.email).toBe('new.email@test.com');
+    expect(updatedUserTwo.lastName).toBe(userDataTwo.lastName);
+  });
 
-        expect(getUserTwo.firstName).toBe(userDataTwo.firstName);
-        expect(getUserTwo.email).toBe(userDataTwo.email);
-        expect(getUserTwo.lastName).toBe(userDataTwo.lastName);
-    });
+  test('should delete a user by id', async () => {
+    await User.removeUserById(createdUserTwo._id);
+    const allUsersAfterDelete = await User.getAllUsers();
+    const userIdsAfterDelete = allUsersAfterDelete.map(user => user._id);
 
-    test('should get all users', async () => {
-        const allUsers = await User.getAllUsers();
-        const userIds = allUsers.map((user) => user._id);
-
-        expect(allUsers).toHaveLength(2);
-        expect(userIds)
-            .toEqual(expect.arrayContaining([createdUserTwo._id, createdUserOne._id]));
-    });
-
-    test('should update a user by id', async () => {
-        const updatedUserTwo = await User.updateUserById(createdUserTwo._id, {
-            email: 'new.email@test.com',
-        });
-
-        expect(updatedUserTwo.firstName).toBe(userDataTwo.firstName);
-        expect(updatedUserTwo.email).toBe('new.email@test.com');
-        expect(updatedUserTwo.lastName).toBe(userDataTwo.lastName);
-    });
-
-    test('should delete a user by id', async () => {
-        await User.removeUserById(createdUserTwo._id);
-        const allUsersAfterDelete = await User.getAllUsers();
-        const userIdsAfterDelete = allUsersAfterDelete.map((user) => user._id);
-
-        expect(allUsersAfterDelete).toHaveLength(1);
-        expect(userIdsAfterDelete)
-            .not.toEqual(expect.arrayContaining([createdUserTwo._id, createdUserOne._id]));
-        expect(userIdsAfterDelete).toEqual(expect.arrayContaining([createdUserOne._id]));
-    });
+    expect(allUsersAfterDelete).toHaveLength(1);
+    expect(userIdsAfterDelete).not.toEqual(
+      expect.arrayContaining([createdUserTwo._id, createdUserOne._id])
+    );
+    expect(userIdsAfterDelete).toEqual(
+      expect.arrayContaining([createdUserOne._id])
+    );
+  });
 });
